85611755afd4f6f83562076e4efa83553d146e1a
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-03-31 04:40:36 +0530

    added: connection with database

--- src/main/java/com/blz/Day_34_Programs/EmployeePayrollData.java
+++ src/main/java/com/blz/Day_34_Programs/EmployeePayrollData.java
@@ -1,8 +1,26 @@
 package com.blz.Day_34_Programs;
 
+import java.time.LocalDate;
+
 public class EmployeePayrollData {
-	public static void main(String[] args) {
-		System.out.println("welcome to JDBC programs");
+
+	public int employeeId;
+	public String employeeName;
+	public double employeeSalary;
+	public LocalDate startDate;
+
+	public EmployeePayrollData(Integer id, String name, Double salary, LocalDate date) {
+
+		this.employeeId = id;
+		this.employeeName = name;
+		this.employeeSalary = salary;
+		this.startDate = date;
+	}
+
+	@Override
+	public String toString() {
+
+		return "EmployeeId: " + employeeId + ", EmployeeName: " + employeeName + ", EmployeeSalary: " + employeeSalary;
 	}
 
 }
